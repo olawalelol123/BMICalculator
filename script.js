9d8bc1c292f073128302bd9427efc7067e428b79
olawalelol123 <olawaleolatunjilol@gmail.com>
2023-01-31 01:09:49 -0800

    first commit

--- script.js
+++ script.js
@@ -1,11 +1,32 @@
-let person = 'Sheriff';
-let bestFriend = 'Olawale';
-let school = 'Sunny Luba Secondary';
-let game = 'Football'
-let age = '20';
-let statement = `Hi, My name is ${person} and my closest friend is ${bestFriend}. We both attended ${school} and we are both born on ${new Date().getFullYear()- age}. We love to play ${game} whenever we go out together.`
+    let height = document.getElementById('height')
+    let weight = document.getElementById('weight')
+    let btn = document.querySelector('input[type = "button"]')
+    let status = document.getElementById('status')
 
-let show = document.querySelector('p')
-show.textContent = statement
-document.body.style.backgroundColor = "#ff6347"
-document.getElementById("about").style.color = '#ffffff';
+    let head2 = document.createElement('h2')
+    let para = document.createElement('p')
+
+    status.appendChild(head2)
+    status.appendChild(para)
+
+    function checkBMI(){
+        let bmi = Number(weight.value)/(Math.pow(height.value/100,2))
+
+        // obesity > 30
+        // overweight >= 25
+        // healthy weight 18.5 - 24.9
+        // underweight < 18.5  
+
+        bmi >= 30 ?
+        head2.textContent = `${bmi} is obesity`
+
+        : bmi >= 25 ?
+        head2.textContent = 'Overweight'
+        : bmi >= 18.5 ?
+        head2.textContent = 'Healthy Weight'
+        : head2.textContent = "Underweight"
+
+        console.log(bmi.toFixed(2))
+    }
+
+    btn.addEventListener('click', checkBMI)
